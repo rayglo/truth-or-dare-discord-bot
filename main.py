ff831f7c68086eef97b763fb013f1e9a1376ba4a
rayglo <41805546+rayglo@users.noreply.github.com>
2022-08-15 00:28:48 +0200

    minor fix

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ c.close()
                  ),
              ])
 async def truth(ctx: interactions.CommandContext, question_type: str = None):
-    print(f"{ctx.user.username} requested question in {ctx.guild.name}. Type: {question_type}")
+    print(f"{str(ctx.user.username)} requested question in {str(ctx.guild.name)}. Type: {question_type}")
     if question_type is None or (question_type != "chill" and question_type != "reflective"):
         question_type = random.choice(["chill", "reflective"])
         print(f"Type assigned: {question_type}")
