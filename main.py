124438a9be4cddb0420b6c470716793de6cd79b8
rayglo <41805546+rayglo@users.noreply.github.com>
2022-08-14 06:44:16 +0200

    minor adjustments

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import random
 import interactions
 import mysql.connector
 
-MEMORY = 10
-
 bot = interactions.Client(token=os.getenv("token"))
 db = mysql.connector.connect(host=os.getenv("db_host"),
                              user=os.getenv("db_user"),
@@ -31,7 +29,7 @@ async def truth(ctx: interactions.CommandContext):
     while n_rand in recent_questions_2:
         n_rand = random.randrange(1, ((n_truths-1)*10)+2, 10)
     print(f"picked question: {n_rand}")
-    if len(recent_questions_2) >= MEMORY:
+    if len(recent_questions_2) >= 10:
         recent_questions_2.pop()
     recent_questions_2.insert(0, n_rand)
     converted_questions = [str(x) for x in recent_questions_2]
@@ -58,14 +56,13 @@ async def on_guild_create(ctx: interactions.api.Guild):
     result = c.fetchone()[0]
     c.close()
     c = db.cursor()
-    memory_array = ",".join(["0" for x in range(MEMORY)])
     if result == 0:
         query2 = "INSERT INTO servers(server_id,server_name,array_latest) VALUES (%s,%s,%s);"
-        c.execute(query2, (str(ctx.id), str(ctx.name), memory_array))
+        c.execute(query2, (str(ctx.id), str(ctx.name), "0,0,0,0,0,0,0,0,0,0"))
         print(f"Inserting id: {ctx.id}, name: {ctx.name}")
     else:
         query2 = "UPDATE servers SET server_name = %s, array_latest = %s WHERE server_id=%s;"
-        c.execute(query2, (str(ctx.name), memory_array, str(ctx.id)))
+        c.execute(query2, (str(ctx.name), "0,0,0,0,0,0,0,0,0,0", str(ctx.id)))
         print(f"Updating id: {ctx.id}, name: {ctx.name}")
     db.commit()
     c.close()
